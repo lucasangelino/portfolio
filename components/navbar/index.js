b732139e3c7d698c0a4735a18024bb1581315961
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-10-26 19:19:34 -0300

    MIU add navbar

---
+++ components/navbar/index.js
@@ -0,0 +1,58 @@
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Container,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Container maxW="container.xl">
+        <Box px={4}>
+          <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+            <Box>Logo</Box>
+
+            <Flex alignItems={"center"}>
+              <Stack direction={"row"} spacing={7}>
+                <Button onClick={toggleColorMode}>
+                  {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                </Button>
+              </Stack>
+            </Flex>
+          </Flex>
+        </Box>
+      </Container>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,22 @@
 import { Container, Flex, VStack } from "@chakra-ui/layout";
 
 // components
-import { Cart } from "../components/cart/";
-import { Details } from "../components/detail/";
+import Navbar from "../components/navbar";
 
 // styles
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <Container maxW="container.xl" p={0}>
-      <Flex
-        h={{ base: "auto", md: "100vh" }}
-        py={[0, 10, 20]}
-        direction={{ base: "column-reverse", md: "row" }}
-      >
-        <Details />
-        <Cart />
-      </Flex>
-    </Container>
+    <>
+      <Navbar />
+      <Container maxW="container.xl" p={0}>
+        <Flex
+          h={{ base: "auto", md: "100vh" }}
+          py={[0, 10, 20]}
+          direction={{ base: "column-reverse", md: "row" }}
+        ></Flex>
+      </Container>
+    </>
   );
 }
