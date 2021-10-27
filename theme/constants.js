9ee117603b195f5647756a68dc0a9b5971d5a45a
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-10-27 18:34:47 -0300

    MIU add proyects

---
+++ theme/constants.js
@@ -0,0 +1,14 @@
+const brandColor = {
+  BRAND_50: "brand.50",
+  BRAND_100: "brand.100",
+  BRAND_200: "brand.200",
+  BRAND_300: "brand.300",
+  BRAND_400: "brand.400",
+  BRAND_500: "brand.500",
+  BRAND_600: "brand.600",
+  BRAND_700: "brand.700",
+  BRAND_800: "brand.800",
+  BRAND_900: "brand.900",
+};
+
+export { brandColor };
