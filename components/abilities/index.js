b08c008fd25c820610149be16ad37ac2885a0fdd
lucasangelino <angelino.n.lucas@gmail.com>
2021-12-21 11:42:27 -0300

    MIU change icons svg and add abilities section

--- components/abilities/index.js
+++ components/abilities/index.js
@@ -1,86 +1,94 @@
-import { Box, Container, Flex, Text } from "@chakra-ui/layout";
+import { Image } from '@chakra-ui/react'
+import { Wrap, Container, WrapItem, Text, Center } from "@chakra-ui/layout";
+import Angular from '../icons/Angular'
+import JavaScript from '../icons/JavaScript'
+import React from '../icons/React'
+import Css from '../icons/Css'
+import Docker from '../icons/Docker'
+import Firebase from '../icons/Firebase'
+import Git from '../icons/Git'
+import Html from '../icons/Html'
+import Jest from '../icons/Jest'
+import Mongo from '../icons/Mongo'
+import Npm from '../icons/Npm'
+import Redis from '../icons/Redis'
+
 // styles
 import { brandColor } from "../../theme/constants";
+
 export default function Abilities() {
-  const { BRAND_500, BRAND_800 } = brandColor;
+  const { BRAND_500 } = brandColor;
 
   return (
-    <Container maxW={"7xl"} mt={60}>
-      <Text fontSize="6xl" color={BRAND_500}>
+    <Container maxW={"7xl"} my={60}>
+      <Text fontSize="6xl" color={BRAND_500} mb={20} align={'center'}>
         Mis habiliades 🐱‍🏍
       </Text>
-      <Flex py={["10, 10, 20"]} direction="row">
-        <Box
-          mt="1"
-          border="2px"
-          borderColor={"yellow.500"}
-          borderRadius="md"
-          p={5}
-          m={5}
-        >
-          JavaScript
-        </Box>
-        <Box
-          mt="1"
-          border="2px"
-          borderColor={"yellow.500"}
-          borderRadius="md"
-          p={5}
-          m={5}
-        >
-          HTML
-        </Box>
-        <Box
-          mt="1"
-          border="2px"
-          borderColor={"yellow.500"}
-          borderRadius="md"
-          p={5}
-          m={5}
-        >
-          CSS
-        </Box>
-        <Box
-          mt="1"
-          border="2px"
-          borderColor={"yellow.500"}
-          borderRadius="md"
-          p={5}
-          m={5}
-        >
-          React
-        </Box>
-        <Box
-          mt="1"
-          border="2px"
-          borderColor={"yellow.500"}
-          borderRadius="md"
-          p={5}
-          m={5}
-        >
-          Angular
-        </Box>
-        <Box
-          mt="1"
-          border="2px"
-          borderColor={"yellow.500"}
-          borderRadius="md"
-          p={5}
-          m={5}
-        >
-          npm
-        </Box>
-        <Box
-          mt="1"
-          border="2px"
-          borderColor={"yellow.500"}
-          borderRadius="md"
-          p={5}
-          m={5}
-        >
-          Docker
-        </Box>
-      </Flex>
+      <Center>
+      <Wrap spacing='60px'>
+
+        {/* Javascript */}
+        <WrapItem>
+          <JavaScript width={100} height={100} />
+        </WrapItem>
+
+        {/* Angular */}
+        <WrapItem>
+          <Angular width={100} height={100} />
+        </WrapItem>
+
+        {/* React */}
+        <WrapItem>
+          <React width={100} height={100} />
+        </WrapItem>
+
+        {/* Css */}
+        <WrapItem>
+          <Css width={100} height={100} />
+        </WrapItem>
+
+        {/* Docker */}
+        <WrapItem>
+          <Docker width={100} height={100} />
+        </WrapItem>
+
+        {/* Firebase */}
+        <WrapItem>
+          <Firebase width={100} height={100} />
+        </WrapItem>
+
+        {/* Git */}
+        <WrapItem>
+          <Git width={100} height={100} />
+        </WrapItem>
+
+        {/* Html */}
+        <WrapItem>
+          <Html width={100} height={100} />
+        </WrapItem>
+
+        {/* Jest */}
+        <WrapItem>
+          <Jest width={100} height={100} />
+        </WrapItem>
+
+        {/* Mongo */}
+        <WrapItem>
+          <Mongo width={100} height={100} />
+        </WrapItem>
+
+        {/* Npm */}
+        <WrapItem>
+          <Npm width={100} height={100} />
+        </WrapItem>
+
+        {/* Redis */}
+        <WrapItem>
+          <Redis width={100} height={100} />
+        </WrapItem>
+        
+      </Wrap>
+      </Center>
     </Container>
   );
 }
--- components/icons/JavaScript.js
+++ components/icons/JavaScript.js
@@ -1,18 +1,13 @@
-import * as React from "react";
+const JavaScript = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    aria-label="JavaScript"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <rect width={512} height={512} fill="#f7df1e" rx="15%" />
+    <path d="M324 370c10 17 24 29 47 29 20 0 33-10 33-24 0-16-13-22-35-32l-12-5c-35-15-58-33-58-72 0-36 27-64 70-64 31 0 53 11 68 39l-37 24c-8-15-17-21-31-21s-23 9-23 21c0 14 9 20 30 29l12 5c41 18 64 35 64 76 0 43-34 67-80 67-45 0-74-21-88-49zm-170 4c8 13 14 25 31 25 16 0 26-6 26-30V203h48v164c0 50-29 72-72 72-39 0-61-20-72-44z" />
+  </svg>
+)
 
-function JavaScript(props) {
-  return (
-    <svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" {...props}>
-      <g fill="none" fillRule="evenodd">
-        <path fill="#F1DC50" d="M0 0h24v24H0z" />
-        <path
-          stroke="#333"
-          strokeWidth={2}
-          d="M12 11v8c0 .876-.523 2-2 2-2.385 0-2.5-2-2.5-2m13.29-5.484c-.6-1.01-1.396-1.516-2.386-1.516C16.856 12 16 13 16 14s.5 2 2.508 2.5c1.278.318 2.492 1 2.492 2.5s-1.315 2-2.5 2c-1.514 0-2.514-.667-3-2"
-        />
-      </g>
-    </svg>
-  );
-}
-
-export default JavaScript;
+export default JavaScript
