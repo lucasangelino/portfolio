8bc3d267aada1eb9e585d695a20c3423d50ac0fe
lucasangelino <angelino.n.lucas@gmail.com>
2022-02-14 17:44:36 -0300

    MIU rm projects

--- pages/services/projects.js
+++
@@ -1,5 +0,0 @@
-export default async function getProject({id}) {
-    const response = await fetch(`https://api.github.com/repos/${id}`);
-    projects = await response.json();
-    return projects;
-}
